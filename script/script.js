60fe60b5c90139b0a16cf346f5d9930ccaa93439
Vitja Nosa <99064218@mydavinci.nl>
2020-12-02 13:16:43 +0100

    duck hunt
    duck hunt is ready to be turned in, but could use some small changes.

--- script/script.js
+++ script/script.js
@@ -3,14 +3,21 @@ var arrayDirection = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
 var duck = document.getElementById("duck");
 var stageImage = document.getElementById("stageImage");
 var gameOverScreen = document.getElementById("gameOverScreen");
+var stage = document.getElementById("stage");
 
 var scoreboard = document.getElementById("scoreboard");
 var scoreboardHit = document.getElementById("hitText");
 var scoreboardMiss = document.getElementById("missText");
 var finalScoreHitText = document.getElementById("finalScoreHitText");
 var finalScoreMissText = document.getElementById("finalScoreMissText");
+var gameplayFeedback = document.getElementById("gameplayFeedback");
 
 var retryButton = document.getElementById("retryButton");
+
+var arrayStartPositionX = [-10, 65, 140, 215, 290, 365, 440, 515, 590, 665, 740, 815, 890, 965, 1040];
+//                          0    1   2    3    4    5    6    7    8    9    10   11   12   13   14
+var arrayStartPositionY = [485, 410, 335, 260, 185, 110, 35, -40];
+//                          0    1    2    3    4    5    6    7
 
 var positionX = 515;
 var positionY = 260;
@@ -19,7 +26,7 @@ var hitScore = 0;
 var missScore = 0;
 var totalScore = 0;
 
-var clicked = true;
+var clicked = false;
 
 var duckRepeat = setInterval(moveDuck, 500);
 
@@ -28,7 +35,7 @@ duck.addEventListener("click", function(){
 	hitScore = hitScore + 1;
 	scoreboardHit.innerHTML = hitScore;
 	gameOver();
-	clicked = false;
+	clicked = true;
 })
 stageImage.addEventListener("click", function(){
 	missScore = missScore + 1;
@@ -39,17 +46,19 @@ retryButton.addEventListener("click", function(){
 	location.reload();
 })
 function moveDuck() {
-	if (clicked == true) {
+	if (clicked == false) {
 		var randomNumber = Math.floor((Math.random() * 8));
 		fly(arrayDirection[randomNumber]);
 	}
 	else {
-		duck.style.left = "515px"
-		duck.style.top = "260px"
+		var randomPositionX = Math.floor((Math.random() * arrayStartPositionX.length));
+		var randomPositionY = Math.floor((Math.random() * arrayStartPositionY.length));
+		duck.style.left = arrayStartPositionX[randomPositionX] + "px";
+		duck.style.top = arrayStartPositionY[randomPositionY] + "px";
 		duck.style.display = "inline";
-		positionX = 515;
-		positionY = 260;
-		clicked = true;
+		positionX = arrayStartPositionX[randomPositionX];
+		positionY = arrayStartPositionY[randomPositionY];
+		clicked = false;
 	}	
 }
 function gameOver() {
@@ -57,10 +66,23 @@ function gameOver() {
 	if (totalScore >= 20) {
 		clearInterval(duckRepeat);
 		gameOverScreen.style.display = "block";
-		duck.style.display = "none";
+		stage.style.display = "none";
 		scoreboard.style.display = "none";
 		finalScoreHitText.innerHTML = hitScore;
 		finalScoreMissText.innerHTML = missScore;
+		if (hitScore <= 5) {
+			gameplayFeedback.innerHTML = "Are you playing on a controller <br> or are you just not trying?";
+		}
+		else if (hitScore > 5 && hitScore <= 12) {
+			gameplayFeedback.innerHTML = "Good not great.";
+		}
+		else if (hitScore > 12 && hitScore < 20) {
+			gameplayFeedback.innerHTML = "You're really good at this!";
+		}
+		else {
+			gameplayFeedback.innerHTML = "Ducks will get nightmares <br> about you.";
+		}
+		
 	}
 }
 function fly(direction) {
